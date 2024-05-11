5edf0f3274c3cd8980345507d880c583ac4fad63
Binnukrishna <binnukrishna5233@gmail.com>
2024-05-11 12:22:50 +0530

    footer added

--- src/javascript/nav_fun.js
+++ src/javascript/nav_fun.js
@@ -3,12 +3,14 @@ document.addEventListener("DOMContentLoaded",()=>{
     const ham = document.querySelector("nav .nav_container .mob_ham")
     const ham_els = document.querySelectorAll("nav .nav_container .mob_ham div")
     const mob_nav = document.querySelector("nav .nav_container .mob_nav_links")
+    const mob_nav_links = document.querySelector("nav .nav_container .mob_nav_links ul")
 
     ham.addEventListener("click",()=>{
         ham_els.forEach(el=>{
             el.classList.toggle("open")
         })
         mob_nav.classList.toggle("open")
+        mob_nav_links.classList.toggle("open")
     })
 
 
